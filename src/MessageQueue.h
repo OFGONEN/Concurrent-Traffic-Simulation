82658ae05ac73ac6af3f11d0859630c400fcafff
ofgonen <ofgonen@gmail.com>
2025-01-22 01:55:39 +0300

    TrafficLight implemented

--- src/MessageQueue.h
+++ src/MessageQueue.h
@@ -17,4 +17,23 @@ private:
   std::deque<T> _queue;
 };
 
+template <typename T> void MessageQueue<T>::send(T &&msg) {
+
+  std::lock_guard<std::mutex> uLock(_mutex);
+
+  _queue.push_back(std::move(msg));
+  _cond.notify_one();
+}
+
+template <typename T> T MessageQueue<T>::receive() {
+
+  std::unique_lock<std::mutex> uLock(_mutex);
+  _cond.wait(uLock, [this] { return !_queue.empty(); });
+
+  T msg = std::move(_queue.back());
+  _queue.pop_back();
+
+  return msg;
+}
+
 #endif
